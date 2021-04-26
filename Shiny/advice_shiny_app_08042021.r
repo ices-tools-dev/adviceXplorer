1a301ef3c2cedc19b13f969f14512d4205cbae4d
Luca Lamoni <32708094+lucalamoni@users.noreply.github.com>
2021-04-26 18:45:41 +0200

    changed paths

--- Shiny/advice_shiny_app_08042021.r
+++ Shiny/advice_shiny_app_08042021.r
@@ -23,12 +23,13 @@ library(tidyverse)
 
 
 ################################
-
+# set working directory
+setwd("D:/Profile/Documents/GitHub/online-advice")
 
 # sources
-source("D:/Profile/Documents/GitHub/VISA_tool/R/explorationVisa/Shiny/utilities_plotting.r")
-source("D:/Profile/Documents/GitHub/VISA_tool/R/explorationVisa/Shiny/ui_08042021.r")
-source("D:/Profile/Documents/GitHub/VISA_tool/R/explorationVisa/Shiny/server_08042021.r")
+source("Shiny/utilities_plotting.r")
+source("Shiny/ui_08042021.r")
+source("Shiny/server_26042021.r")
 
 
 
