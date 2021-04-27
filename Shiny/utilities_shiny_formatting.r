713aed908e253ce3704eb4fb29f1de66f5e4f4af
Luca Lamoni <32708094+lucalamoni@users.noreply.github.com>
2021-04-27 15:44:16 +0200

    new app version, plots included

---
+++ Shiny/utilities_shiny_formatting.r
@@ -0,0 +1,39 @@
+side_width <- 5
+
+# allocations advice information panel
+allocations_infopanel <-
+  sidebarPanel(
+    width = side_width
+  )
+
+# advice plot side panel
+allocations_plotspanel <-
+  mainPanel(
+    width = 12 - side_width,
+    tabsetPanel(
+        tabPanel(
+            title = "Catches",
+            plotlyOutput("catches"),
+            h5(helpText("Figure 1: Catches"))
+            # actionButton("r_catches", "Get Stock Data")
+        ),
+        tabPanel(
+            title = "Recruitment",
+            plotlyOutput("R"),
+            h5(helpText("Figure 2: Stock recruitment"))
+            # actionButton("r_recr", "Get Stock Data")
+        ),
+        tabPanel(
+            title = "Fi",
+            plotlyOutput("f"),
+            h5(helpText("Figure 3: Fish mortality"))
+            # actionButton("r_f", "Get Stock Data")
+        ),
+        tabPanel(
+            title = "SSB",
+            plotlyOutput("SSB"),
+            h5(helpText("Figure 4: SSB"))
+            # actionButton("r_SSB", "Get Stock Data")
+        )
+    )
+  )
