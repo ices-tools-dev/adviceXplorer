15a43eae040d2791673b86b945385b37094f2d59
Luca <lucalamoni@gmail.com>
2021-08-23 16:29:34 +0200

    ui temp

--- temp/ui.r
+++ temp/ui.r
@@ -30,17 +30,26 @@ ecoregion = "Celtic Seas Ecoregion"
 # Load the lighter version of the ecoregions shapefile
 shape_eco <- st_read(dsn = "test_lowres", 
     layer = "ecoR_lowres")
+
+shape_ices_areas <- st_read(dsn = "ICES_areas_low_res", 
+    layer = "ICES_areas_low_res")
 # Change one Ecoregion name (this comes handy when we filter the stock list table)
 levels(shape_eco$Ecoregion)[match("Icelandic Waters",levels(shape_eco$Ecoregion))] <- "Iceland Sea"
 
 # Add an id to each ecoregion (this potentially can be eliminated because the ecoregions in the shape file have already an id)
 shape_eco$uid <- paste0("P", 1:17)
 
+minZoom = 0
+maxZoom = 13
+resolutions <- 2*(2^(maxZoom:minZoom))
+crs_laea <- leafletCRS(crsClass = "L.Proj.CRS", code = "EPSG:3035",
+  proj4def = "+proj=laea +x_0=0 +y_0=0 +lon_0= -1.235660 +lat_0=60.346958",
+  resolutions = resolutions)
 
-source("../Shiny/utilities_shiny_formatting.r")
-source("../Shiny/utilities_plotting.r")
-source("../Shiny/utilities_sag_data.r")
-source("../Shiny/utilities_shiny_Input.r")
+source("D:/Profile/Documents/GitHub/online-advice/Shiny/utilities_shiny_formatting.r")
+source("D:/Profile/Documents/GitHub/online-advice/Shiny/utilities_plotting.r")
+source("D:/Profile/Documents/GitHub/online-advice/Shiny/utilities_sag_data.r")
+source("D:/Profile/Documents/GitHub/online-advice/Shiny/utilities_shiny_Input.r")
 
 
 
