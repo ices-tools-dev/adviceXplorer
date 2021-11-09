a5a4770b3011fcdb1a777027214f7c96cacb354e
Luca <lucalamoni@gmail.com>
2021-11-09 10:34:11 +0100

    fixed repo url hyperlink

--- temp/ui.r
+++ temp/ui.r
@@ -88,6 +88,6 @@ navbarPage(
     position = "fixed-top",
 
     tags$script(HTML("var header = $('.navbar > .container-fluid');
-header.append('<div style=\"float:right\"><a href=\"https://https://github.com/ices-tools-dev/online-advice\"><img src=\"GitHub-Mark-32px.png\" alt=\"alt\" style=\"margin-top: -14px; padding-right:5px;padding-top:25px;\"></a></div>');
+header.append('<div style=\"float:right\"><a href=\"https://github.com/ices-tools-dev/online-advice\"><img src=\"GitHub-Mark-32px.png\" alt=\"alt\" style=\"margin-top: -14px; padding-right:5px;padding-top:25px;\"></a></div>');
 console.log(header)"))
 )
