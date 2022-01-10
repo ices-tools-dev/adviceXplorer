b8db59e3b90b29b7c8a64ee1befd4928932b7840
Luca <lucalamoni@gmail.com>
2022-01-10 10:17:28 +0100

    fixed bug in ui related to div

--- temp/ui.r
+++ temp/ui.r
@@ -1,9 +1,8 @@
 
-
 navbarPage(
     # tab title
     windowTitle = "TAF Advice Tool",
-
+    
     # navbar title
     title =
         shiny::div(img(
@@ -17,29 +16,10 @@ navbarPage(
         sidebarLayout(
             sidebarPanel = maps_panels,
             mainPanel = selectize_panel
-            # Top panel with widgets sold
-            # wellPanel(
-            #     textOutput("Ecoregion")
-            # ),
-
-            # the map itself
-            # sidebarPanel(
-            #     div(
-            #         class = "outer",
-            #         tags$style(type = "text/css", ".outer {position: fixed; top: 61px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
-            #         # width = side_width,
-            #         leafletOutput("map", width = "35%", height = "100%")
-            #     )
-            # ),
-            # mainPanel(
-            #     # width = 8,
-            #     # div(class="outer",
-            #     # tags$style(type = "text/css", ".outer {position: fixed; top: 61px; left: 500px; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
-            #     # DTOutput("tbl")
-            # )
         )
         # )
     ),
+    
     tabPanel(
         "Stock Selection",
         DTOutput("tbl")#,
@@ -54,6 +34,7 @@ navbarPage(
         )
         # includeMarkdown("Instructions.Rmd")
     ),
+    
     tabPanel(
         "Catch Options & Advice",
         
@@ -63,35 +44,42 @@ navbarPage(
                 # useShinyjs(),
                 # inlineCSS(list("table2" = "font-size: 10px"))
             ),
+            # browser(),
+            # mainPanel(# this is not running 
+            #     width = 9,
+            #         htmlOutput("Advice_Sentence"),
+            #         div(
+            #         class = "outer",
+            #         tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
+            #         plotlyOutput("catch_scenario_plot_1", width = "100%", height = "100%")),
+            #         # plotlyOutput("catch_scenario_plot_2"),
+            #         div(
+            #         class = "outer",
+            #         tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
+            #         plotlyOutput("catch_scenario_plot_2",width = "100%", height = "100%")),
+            #         # DTOutput("catch_scenario_table")
+            #         div(
+            #         class = "outer",
+            #         tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
+            #         DTOutput("catch_scenario_table",width = "100%", height = "100%"))
+            #         )
             mainPanel(
                 width = 9,
-                    htmlOutput("Advice_Sentence"),
-                    div(
-                    class = "outer",
-                    tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
-                    plotlyOutput("catch_scenario_plot_1",width = "100%", height = "100%")),
-                    # plotlyOutput("catch_scenario_plot_2"),
-                    div(
-                    class = "outer",
-                    tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
-                    plotlyOutput("catch_scenario_plot_2",width = "100%", height = "100%")),
-                    # DTOutput("catch_scenario_table")
-                    div(
-                    class = "outer",
-                    tags$style(type = "text/css", ".outer {position: relative; top: 61px; left: 0; right: 0; bottom: 61px; overflow: hidden; padding: 50}"),
-                    DTOutput("catch_scenario_table",width = "100%", height = "100%"))
-                    
-                
-
+                plotlyOutput("catch_scenario_plot_1"),
+                plotlyOutput("catch_scenario_plot_2"),
+                DTOutput("catch_scenario_table")
             )
         
         # verbatimTextOutput("In_Construction")
     ),
+    
+
     tabPanel(
         "Resources",
         verbatimTextOutput("headline")
     )
-    ),
+    ),# close tabsetpanel
+    
     # extra tags, css etc
     tags$style(type = "text/css", "li {font-size: 20px;}"),
     tags$style(type = "text/css", "p {font-size: 21px;}"),
@@ -99,7 +87,6 @@ navbarPage(
     tags$head(tags$style(HTML("#go{background-color:#14c6dd}"))), ##dd4814 0range
     theme = shinytheme("united"),  ##### need to work on this, the orange is part of the css theme united, check bslib in forked repo
     position = "fixed-top",
-
     tags$script(HTML("var header = $('.navbar > .container-fluid');
 header.append('<div style=\"float:right\"><a href=\"https://github.com/ices-tools-dev/online-advice\"><img src=\"GitHub-Mark-32px.png\" alt=\"alt\" style=\"margin-top: -14px; padding-right:5px;padding-top:25px;\"></a></div>');
 console.log(header)"))
