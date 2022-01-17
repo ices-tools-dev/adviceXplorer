68b79f5f5bbcdb86e147bf7712454acdbb398402
Luca <lucalamoni@gmail.com>
2022-01-17 16:31:26 +0100

    plots responsive

--- Shiny/utilities_shiny_formatting.r
+++ Shiny/utilities_shiny_formatting.r
@@ -4,11 +4,14 @@ side_width <- 4
 allocations_infopanel <-
   sidebarPanel(
     width = 8,
-    panel(title = "plots",
-    plotlyOutput("all_plots", height = 800),
-    h5(helpText("Stock Development over time"))
+    panel(
+      title = "plots",
+      fillPage(
+        tags$style(type = "text/css", "#all_plots {height: calc(100vh - 200px) !important;}"),
+        plotlyOutput("all_plots", height = "100%", width = "100%")
+      ),
+      h5(helpText("Stock Development over time"))
     )
-
   )
 
 # advice plot side panel
@@ -42,10 +45,13 @@ allocations_plotspanel <-
     #     ),
     #     tabPanel(
       panel(
-            title = "Quality of Assessment",
-            plotlyOutput("Q_Ass",height = 800),
-            h5(helpText("Quality of Assessment"))
-            # actionButton("r_SSB", "Get Stock Data")
+        title = "Quality of Assessment",
+        fillPage(
+          tags$style(type = "text/css", "#Q_Ass {height: calc(100vh - 200px) !important;}"),
+          plotlyOutput("Q_Ass", height = "100%", width = "100%")
+        ),
+        h5(helpText("Quality of Assessment"))
+        # actionButton("r_SSB", "Get Stock Data")
       )
         # )
     # ),
@@ -82,69 +88,70 @@ maps_panels <-
     )
   )
 
-selectize_panel <- 
-mainPanel(
-  width = 4,
-  panel(
-        selectizeInput(
-            inputId = "selected_locations",
-            label = "ICES Ecoregions",
-            choices = shape_eco$Ecoregion,
-            selected = NULL,
-            multiple = TRUE,
-            options = list(
-              placeholder = 'Select Ecoregion(s)')
-        ),
-        selectizeInput(
-            inputId = "selected_areas",
-            label = "ICES Areas",
-            choices = ices_areas$Area_Full,
-            selected = NULL,
-            multiple = TRUE,
-            options = list(
-              placeholder = 'Select ICES Area(s)')
-        ),
-        #######
-        selectizeInput(
-            inputId = "selected_years",
-            label = "Year SID/SAG",
-            choices = Years$Year,
-            selected = 2021,
-            multiple = FALSE,
-            options = list(
-              placeholder = 'Select ICES Area(s)')
-        ),
-        #######
-        
-          selectizeGroupUI(
-            id = "my-filters",
-            params = list(
-              # EcoRegion = list(inputId = "EcoRegion", title = "EcoRegion:"),
-              # StockDatabaseID = list(inputId = "StockDatabaseID", title = "StockDatabaseID:"),
-              # StockKey = list(inputId = "StockKey", title = "StockKey:"),
-              StockKeyLabel = list(inputId = "StockKeyLabel", title = "StockKeyLabel:"),
-              # SpeciesScientificName = list(inputId = "SpeciesScientificName", title = "SpeciesScientificName:"),
-              SpeciesCommonName = list(inputId = "SpeciesCommonName", title = "SpeciesCommonName:"),
-              ExpertGroup = list(inputId = "ExpertGroup", title = "ExpertGroup:"),
-              # AdviceDraftingGroup = list(inputId = "AdviceDraftingGroup", title = "AdviceDraftingGroup:"),
-              DataCategory = list(inputId = "DataCategory", title = "DataCategory:"),
-              YearOfLastAssessment = list(inputId = "YearOfLastAssessment", title = "YearOfLastAssessment:"),
-              # AssessmentFrequency = list(inputId = "AssessmentFrequency", title = "AssessmentFrequency:"),
-              # YearOfNextAssessment = list(inputId = "YearOfNextAssessment", title = "YearOfNextAssessment:"),
-              # AdviceReleaseDate = list(inputId = "AdviceReleaseDate", title = "AdviceReleaseDate:"),
-              AdviceCategory = list(inputId = "AdviceCategory", title = "AdviceCategory:"),
-              # AdviceType = list(inputId = "AdviceType", title = "AdviceType:"),
-              # TrophicGuild = list(inputId = "TrophicGuild", title = "TrophicGuild:"),
-              # FisheriesGuild = list(inputId = "FisheriesGuild", title = "FisheriesGuild:"),
-              # SizeGuild = list(inputId = "SizeGuild", title = "SizeGuild:"),
-              Published = list(inputId = "Published", title = "Published:")
-              # ICES_area = list(inputId = "ICES_area", title = "ICES_area")
-            ),
-            inline = FALSE
-          ), 
-          heading = "Data filtering",
-          status = "primary"
+selectize_panel <-
+  mainPanel(
+    width = 4,
+    panel(
+      selectizeInput(
+        inputId = "selected_locations",
+        label = "ICES Ecoregions",
+        choices = shape_eco$Ecoregion,
+        selected = NULL,
+        multiple = TRUE,
+        options = list(
+          placeholder = "Select Ecoregion(s)"
         )
-)
+      ),
+      selectizeInput(
+        inputId = "selected_areas",
+        label = "ICES Areas",
+        choices = ices_areas$Area_Full,
+        selected = NULL,
+        multiple = TRUE,
+        options = list(
+          placeholder = "Select ICES Area(s)"
+        )
+      ),
+      #######
+      selectizeInput(
+        inputId = "selected_years",
+        label = "Year SID/SAG",
+        choices = Years$Year,
+        selected = 2021,
+        multiple = FALSE,
+        options = list(
+          placeholder = "Select ICES Area(s)"
+        )
+      ),
+      #######
 
-
+      selectizeGroupUI(
+        id = "my-filters",
+        params = list(
+          # EcoRegion = list(inputId = "EcoRegion", title = "EcoRegion:"),
+          # StockDatabaseID = list(inputId = "StockDatabaseID", title = "StockDatabaseID:"),
+          # StockKey = list(inputId = "StockKey", title = "StockKey:"),
+          StockKeyLabel = list(inputId = "StockKeyLabel", title = "StockKeyLabel:"),
+          # SpeciesScientificName = list(inputId = "SpeciesScientificName", title = "SpeciesScientificName:"),
+          SpeciesCommonName = list(inputId = "SpeciesCommonName", title = "SpeciesCommonName:"),
+          ExpertGroup = list(inputId = "ExpertGroup", title = "ExpertGroup:"),
+          # AdviceDraftingGroup = list(inputId = "AdviceDraftingGroup", title = "AdviceDraftingGroup:"),
+          DataCategory = list(inputId = "DataCategory", title = "DataCategory:"),
+          YearOfLastAssessment = list(inputId = "YearOfLastAssessment", title = "YearOfLastAssessment:"),
+          # AssessmentFrequency = list(inputId = "AssessmentFrequency", title = "AssessmentFrequency:"),
+          # YearOfNextAssessment = list(inputId = "YearOfNextAssessment", title = "YearOfNextAssessment:"),
+          # AdviceReleaseDate = list(inputId = "AdviceReleaseDate", title = "AdviceReleaseDate:"),
+          AdviceCategory = list(inputId = "AdviceCategory", title = "AdviceCategory:"),
+          # AdviceType = list(inputId = "AdviceType", title = "AdviceType:"),
+          # TrophicGuild = list(inputId = "TrophicGuild", title = "TrophicGuild:"),
+          # FisheriesGuild = list(inputId = "FisheriesGuild", title = "FisheriesGuild:"),
+          # SizeGuild = list(inputId = "SizeGuild", title = "SizeGuild:"),
+          Published = list(inputId = "Published", title = "Published:")
+          # ICES_area = list(inputId = "ICES_area", title = "ICES_area")
+        ),
+        inline = FALSE
+      ),
+      heading = "Data filtering",
+      status = "primary"
+    )
+  )
