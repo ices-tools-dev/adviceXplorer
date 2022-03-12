08857520c5b8f2f40283a013673b156fdff0c78b
Luca <lucalamoni@gmail.com>
2022-03-12 11:21:03 +0100

    formatted html tag

--- App/ui.r
+++ App/ui.r
@@ -202,8 +202,12 @@ navbarPage(
     theme = shinytheme("cerulean"),  ##### need to work on this, the orange is part of the css theme united, check bslib in forked repo
     position = "fixed-top",
     tags$script(HTML("var header = $('.navbar > .container-fluid');
-header.append('<div style=\"float:right\"><a href=\"https://github.com/ices-tools-dev/online-advice\"><img src=\"GitHub-Mark-32px.png\" alt=\"alt\" style=\"margin-top: -14px; padding-right:5px;padding-top:25px;\"></a></div>')
-console.log(header)"))
+                    header.append('<div style=\"float:right\">
+                    <a href=\"https://github.com/ices-tools-dev/online-advice\">
+                    <img src=\"GitHub-Mark-32px.png\" alt=\"alt\" style=\"margin-top: -14px; padding-right:5px;padding-top:25px;\">
+                    </a>
+                    </div>')
+                    console.log(header)"))
 )
 
 
