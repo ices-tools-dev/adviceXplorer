89e65e5ce4907aa09dcd15611a96dc79a536cb40
Neil Maginnis <neilm@ices.dk>
2022-10-03 16:51:39 +0200

    tidy up server.R

--- App/utilities_mapping.r
+++ App/utilities_mapping.r
@@ -72,3 +72,135 @@ map_ecoregion <- function(shape_eco, eu_shape) {
         hideGroup(group = shape_eco$Ecoregion)
 }
 
+
+#' Crop larger map to smaller map
+#'
+#' Using an optional buffer, reduce a map to the size of the bounding box of a smaller map
+#' 
+#' @param large_map sf object
+#' @param small_map sf object
+#' @param buffer the size of the buffer to be used (m)
+#' @param ... 
+#'
+#' @return
+crop_large_map_to_small <- function(large_map, small_map, buffer=0, ...){
+  
+  buffered_bbox <- small_map %>% sf::st_union() %>%
+    sf::st_buffer(dist = buffer) %>%
+    sf::st_bbox() %>% 
+    sf::st_as_sfc()
+  
+  cropped_map <- large_map %>% sf::st_intersection(buffered_bbox)
+}
+
+
+#' Generates a map containing ices areas together with relevant European landmasses
+#' 
+#' (land masses are cropped and overseas territories do not appear)
+#'
+#' @param buffer_size The size of the buffer used around ices areas (m)
+#' @param resolution_reduction the reduction in resolution expressed as a ratio
+#' @param path 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_eu_map <- function(buffer_size = 1300000, resolution_reduction = 0.15, path){
+  
+  land_masses <- sf::st_read("../../GIS/ne_50m_land/ne_50m_land.shp") 
+  ices_regions <-  sf::st_read(dsn = "App/Data/ICES_areas_low_res/ICES_areas_low_res.shp")
+  land_crs <- sf::st_crs(land_masses)
+  land_masses <- sf::st_transform(land_masses, sf::st_crs(ices_regions))
+  ices_land_areas <- crop_large_map_to_small(large_map = land_masses, small_map = ices_regions, buffer = buffer_size) %>% 
+    rmapshaper::ms_simplify(keep = resolution_reduction, keep_shapes= TRUE) %>% 
+    sf::st_transform(land_crs) %>% 
+    sf::st_combine() %>% 
+    sf::st_segmentize(dfMaxLength = 1000)
+
+  sf::st_write(ices_land_areas, dsn = path, delete_dsn = TRUE)
+}
+# create_eu_map(path = "./App/Data/shape_EU_simplified/shape_EU_simplified2.shp")
+# temp_map <- sf::st_read(dsn = "./App/Data/shape_EU_simplified/shape_EU_simplified2.shp")
+# map_ecoregion(shape_eco, eu_shape = temp_map)
+
+
+#' Server side functionality for the landing page map panel
+#'
+#' @param input 
+#' @param output 
+#' @param server 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+map_panel_server <- function(input, output, server) {
+  
+  # Render Map 1
+  output$map1 <- renderLeaflet({
+    map_ecoregion(shape_eco, eu_shape)
+  }) # END RENDER LEAFLET map1
+  
+  proxy_1 <- leafletProxy("map1")
+  
+  # create empty vector to hold all click ids
+  selected_1 <- reactiveValues(groups = vector())
+  
+  # find index
+  observeEvent(input$map1_shape_click, {
+    
+    ## calculate index of ecoregion selected in shape_eco
+    idx_1 <- match(input$map1_shape_click$id, shape_eco$Ecoregion)
+    
+    if (input$map1_shape_click$group == "Eco_regions") {
+      selected_1$groups <- c(selected_1$groups, input$map1_shape_click$id)
+      
+      proxy_1 %>%
+        showGroup(group = input$map1_shape_click$id) 
+      
+      
+      ## this js code allows for the stock slection tab to be enabled once one coregion is clicked
+      runjs("$(tab).removeClass('disabled');")#%>%
+      
+      
+      # print(match(input$map_shape_click$id, shape_eco$Ecoregion))
+    } else {
+      selected_1$groups <- setdiff(selected_1$groups, input$map1_shape_click$group)
+      proxy_1 %>%
+        hideGroup(group = input$map1_shape_click$group) #%>%
+      
+      
+    }
+    updateSelectizeInput(session,
+                         inputId = "selected_locations",
+                         label = "ICES Ecoregions",
+                         choices = shape_eco$Ecoregion,
+                         selected = selected_1$groups
+    )
+    
+  })
+  
+  observeEvent(input$selected_locations,
+               {
+                 removed_via_selectInput <- setdiff(selected_1$groups, input$selected_locations)
+                 added_via_selectInput <- setdiff(input$selected_locations, selected_1$groups)
+                 
+                 if (length(removed_via_selectInput) > 0) {
+                   selected_1$groups <- input$selected_locations
+                   
+                   proxy_1 %>% hideGroup(group = removed_via_selectInput)
+                 }
+                 
+                 if (length(added_via_selectInput) > 0) {
+                   selected_1$groups <- input$selected_locations
+                   
+                   proxy_1 %>% showGroup(group = added_via_selectInput)
+                   
+                   ## this js code allows for the stock slection tab to be enabled once one coregion is clicked
+                   runjs("$(tab).removeClass('disabled');")
+                 }
+               },
+               ignoreNULL = FALSE
+  )
+}
