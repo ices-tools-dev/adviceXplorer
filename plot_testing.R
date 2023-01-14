093cba446a04abed896bc4401e0a73b5b7045d76
Luca <lucalamoni@gmail.com>
2023-01-14 18:17:32 +0100

    Bugs to fix before launch #153

--- plot_testing.R
+++ plot_testing.R
@@ -23,7 +23,21 @@ year <- 2021
 update_SAG(year)
 update_SID(year)
 
-stock <- c("ple.27.7d", "had.27.46a20", "wit.27.3a47d", "bll.27.3a47de", "fle.27.3a4")[5]
+stock <- c("spr.27.22-32", "her.27.3031", "wit.27.3a47d", "bll.27.3a47de", "fle.27.3a4")[1]
+access_sag_data_local <- function(stock_code, year) {
+#   
+    # Dowload the data
+    df_summary <- fread(sprintf("App/Data/SAG_%s/SAG_summary.csv", year)) ####there is a space after SAG_ fix this below
+    SAGsummary <- df_summary %>% filter(fishstock == stock_code)
+
+    df_refpts <- fread(sprintf("App/Data/SAG_%s/SAG_refpts.csv", year)) ####there is a space after SAG_ fix this below
+    SAGrefpts <- df_refpts %>% filter(StockKeyLabel == stock_code)
+
+    data_sag <- merge(SAGsummary, SAGrefpts)
+    data_sag <- data_sag %>% select(-fishstock) %>% filter(StockPublishNote == "Stock published")
+    
+    return(data_sag)
+}
 df <- access_sag_data_local(stock, year)
 
 # view sag page
