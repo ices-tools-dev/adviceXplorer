b2efba8f420e18d5efd23fd3898a06444262fe61
Luca Lamoni <lucalamoni@gmail.com>
2023-11-24 10:35:14 +0100

    hardcoded SID entried for cod substocks

--- App/update_SID_data.r
+++ App/update_SID_data.r
@@ -20,9 +20,9 @@
 #'
 #' @export
 #'
-update_SID <- function(year){
+update_SID <- function(year) {
     mkdir(paste0("Data/SID_", year))
-    
+
     ### download SID
     stock_list_all <- download_SID(year)
     ### modifify SID table, 1 row == 1 Ecoregion
@@ -30,8 +30,26 @@ update_SID <- function(year){
     ### add hyperlinks to table
     stock_list_long <- sid_table_links(stock_list_long)
 
+    if (year == 2023) {
+        row_to_duplicate <- stock_list_long %>% filter(StockKeyLabel == "cod.27.46a7d20" & stock_list_long$EcoRegion %in% c("Greater North Sea Ecoregion", "Celtic Seas Ecoregion"))
+        # row_to_duplicate <- stock_list_long[stock_list_long$StockKeyLabel == "cod.27.46a7d20" & stock_list_long$EcoRegion == "Greater North Sea Ecoregion" , ]
+        duplicated_df <- row_to_duplicate[rep(row.names(row_to_duplicate), each = 3), ]
+        row.names(duplicated_df) <- NULL # Reset row names
+
+        Cod_Keys <- c(18282, 18283, 18284, 18282, 18283, 18284)
+        for (i in 1:nrow(duplicated_df)) {
+            duplicated_df$AssessmentKey[i] <- Cod_Keys[i]
+        }
+        stock_list_long <- rbind(stock_list_long, duplicated_df)
+        row.names(stock_list_long) <- NULL # Reset row names
+    }
+
+
+
+
+
+
 
     write.taf(stock_list_long, file = "SID.csv", dir = paste0("Data/SID_", year))
-
 }
 # update_SID(2023)
--- dev/plot_testing.R
+++ dev/plot_testing.R
@@ -1239,3 +1239,54 @@ https://sag.ices.dk/SAG_API/api/StockDownload?assessmentKey=18282
 
 
 getAdviceViewRecord(assessmentkey = 18396)
+
+
+df <- data.frame(
+  ID = c(1, 2, 3),
+  Name = c("John", "Alice", "Bob"),
+  Age = c(25, 30, 22)
+)
+
+# Choose the row you want to duplicate (let's say the first row, index 1)
+row_to_duplicate <- df[1, ]
+
+# Duplicate the chosen row
+duplicated_row <- rbind(df, row_to_duplicate)
+
+# Change one value in the duplicated row (let's say change the ID to 4)
+duplicated_row[nrow(duplicated_row), "ID"] <- 4
+
+year <- 2023
+update_SID <- function(year){
+    mkdir(paste0("App/Data/SID_", year))
+    
+    ### download SID
+    stock_list_all <- download_SID(year)
+    ### modifify SID table, 1 row == 1 Ecoregion
+    stock_list_long <- separate_ecoregions(stock_list_all)
+    names(stock_list_long)
+    ### add hyperlinks to table
+    # stock_list_long <- sid_table_links(stock_list_long)
+
+    
+    if (stock_list_long$YearOfLastAssessment == 2023){
+      row_to_duplicate <- stock_list_long %>% filter(StockKeyLabel == "cod.27.46a7d20" & stock_list_long$EcoRegion %in% c("Greater North Sea Ecoregion", "Celtic Seas Ecoregion"))
+      # row_to_duplicate <- stock_list_long[stock_list_long$StockKeyLabel == "cod.27.46a7d20" & stock_list_long$EcoRegion == "Greater North Sea Ecoregion" , ] 
+      duplicated_df <- row_to_duplicate[rep(row.names(row_to_duplicate), each = 3), ]
+      row.names(duplicated_df) <- NULL  # Reset row names
+      
+      Cod_Keys <- c(18282,18283,18284,18282,18283,18284)
+      for (i in 1:nrow(duplicated_df)) {
+        duplicated_df$AssessmentKey[i] <- Cod_Keys[i]
+      }
+    }
+
+    stock_list_long <- rbind(stock_list_long, duplicated_df)
+    row.names(stock_list_long) <- NULL  # Reset row names
+  
+    write.taf(stock_list_long, file = "SID.csv", dir = paste0("App/Data/SID_", year))
+
+}
+
+year <- 2023
+update_SID(2022)
