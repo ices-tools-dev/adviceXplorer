7b66194dec0db3d3a94543bfd450df83cbe3e31a
Luca Lamoni <lucalamoni@gmail.com>
2024-07-16 14:39:16 +0200

    When 2 ecoregions are selected and stock is present in both, the stock info is repeated twice #265

--- App/server.r
+++ App/server.r
@@ -225,8 +225,9 @@ replaced_advice_doi <- eventReactive(req(query$stockkeylabel,query$year), {
 })
 ###### info about the stock selected for top of page
 stock_info <- reactive({
-  filtered_row <- res_mod()[res_mod()$AssessmentKey == query$assessmentkey,] 
-  get_Stock_info(filtered_row$SpeciesCommonName, SAG_data_reactive()$StockKeyLabel[1],  SAG_data_reactive()$AssessmentYear[1], SAG_data_reactive()$StockDescription[1]) #,
+  filtered_row <- res_mod()[res_mod()$AssessmentKey == query$assessmentkey,]
+  get_Stock_info(filtered_row$SpeciesCommonName[1], SAG_data_reactive()$StockKeyLabel[1],  SAG_data_reactive()$AssessmentYear[1], SAG_data_reactive()$StockDescription[1]) #,
+  
 }) 
 
 output$stock_infos1 <- output$stock_infos2 <- output$stock_infos3 <- renderUI(
